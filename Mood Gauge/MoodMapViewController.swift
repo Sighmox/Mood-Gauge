a7700d23ded742234079521391e6ba919b463a24
Sighmox <Sighmox@github.com>
2019-05-08 21:52:06 -0500

    Added in Mood map

---
+++ Mood Gauge/MoodMapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MoodMapViewController.swift
+//  Mood Gauge
+//
+//  Created by Paul Baker on 5/8/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MoodMapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        
+        
+        
+        
+        
+    }
+}
