e6a5a012a6b84da93c06907af56ef7df21a8ab7f
Sighmox <Sighmox@github.com>
2019-05-12 12:57:20 -0500

    Progressing thru moods map does not populate moods

---
+++ Mood Gauge/AnnotationPointManager.swift
@@ -0,0 +1,19 @@
+//
+//  AnnotationPointManager.swift
+//  Mood Gauge
+//
+//  Created by Paul Baker on 5/12/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+
+//extension Moods: MKAnnotation {
+//    public var coordinate: CLLocationCoordinate2D {
+//        guard let latitude = latitude, let longitude = longitude else {
+//            return kCLLocationCoordinate2DInvalid
+//            
+//        }
+//        
+//        let latDegrees = CLLocationDegrees(latitude.fl)
+//    }
+//}
